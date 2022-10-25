03a3b796db24abb288644a7833d8704f31cfd631
javier-bustillo <jjavier.bustillo@gmail.com>
2022-10-25 19:00:50 +0000

    get_member function completed

--- src/datastructures.py
+++ src/datastructures.py
@@ -44,8 +44,9 @@ class FamilyStructure:
         return None
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
